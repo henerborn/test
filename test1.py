ee6e3daa87de9b835a69c4c680a48fd8f5c980be
henerborn <henerborn69@gmail.com>
2025-09-17 00:34:57 +0300

    class inheritance

--- test1.py
+++ test1.py
@@ -1,11 +1,25 @@
-def sheeps(day1, day2, count):
-    all_days = day1 + day2
-    summ = 0
-    for i in range(len(all_days)):
-        summ += all_days[i]
-    return count - summ
+class Character:
+    def __init__(self, health, damage, speed):
+        self.health = health
+        self.damage = damage
+        self.speed = speed
+    def take_damage(self, amount):
+        self.health -= amount
 
-print(sheeps([0], [4, 15], 31))
+class Warrior(Character):
+    def __init__(self, health, damage, speed):
+        super().__init__(health, damage, speed)
+        self.iron_armor = 0.90
+    def take_damage(self, amount):
+        mod_amount = amount * self.iron_armor
+        return super().take_damage(mod_amount)
+    
+
+warrior = Warrior(100, 50, 10)
+print(f"Initial health: {warrior.health}")
+warrior.take_damage(40)
+print(f"Health after damage: {warrior.health}")
+
 
 
   
